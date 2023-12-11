fe8ae73e04a78a8dde1299c9ba4dcdc604fd6f2c
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2023-12-11 14:20:49 +0100

    Create used_stylized_betweenness_functions.R

---
+++ used_stylized_betweenness_functions.R
@@ -0,0 +1,31 @@
+ sb <- function(context){
+   n_objects <- nrow(context)
+   stylized_betweenness <- array(as.logical(FALSE),rep(n_objects,3))
+   for( k in seq_len(n_objects)){
+       intent_1 <- context[k,]
+	   print(k)
+	for( m in seq_len(n_objects)){
+	    intent_2 <- intent_1 & context[m,]#pmin(intent_1,context[m,])
+		extent_2 <- oofos:::compute_phi(intent_2,context)
+		size_extent_2 <- sum(extent_2)
+		for( l in seq_len(n_objects)){
+		   intent_3 <- intent_2 & context[l,]#pmin(intent_2, context[l,])
+		   extent_3 <- oofos:::compute_phi(intent_3,context)
+		   size_extent_3 <- sum(extent_3)
+		   stylized_betweenness[k,l,m] <- size_extent_2/size_extent_3
+		}
+	}
+  }
+return(stylized_betweenness)}
+
+
+sb_geom <- function(X){
+  n_objects <- nrow(X)
+  stylized_betweenness <- array(0,rep(n_objects,3))
+  indexs <- expand.grid(seq_len(n_objects),seq_len(n_objects))
+  for(l in (1:n_objects)){
+    print(l)
+	difference_vectors <- X-X[l,]
+	stylized_betweenness[,l,] <- vecAngle(difference_vectors[indexs$Var1,],difference_vectors[indexs$Var2,])
+	}
+return(stylized_betweenness)}
