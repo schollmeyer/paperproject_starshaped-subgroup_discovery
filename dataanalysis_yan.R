01649667c1b3db41164d1068c10b6fa129b40031
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2023-12-14 11:56:07 +0100

    Update dataanalysis_yan.R

--- dataanalysis_yan.R
+++ dataanalysis_yan.R
@@ -1,1 +1,23 @@
+# needed packages
+if (!require("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("SingleCellExperiment")
+
+sce <- SingleCellExperiment(
+    assays = list(
+        counts = as.matrix(yan),
+        logcounts = log2(as.matrix(yan) + 1)
+    ), 
+    colData = ann
+)
+
+# preprocessing counts (gene filter plus scaling)
+x <- t(counts(sce))
+x <- gene_filter(x)
+x <- log2(1+x)
+x <- scaling(x)
+
+
+absb <- get_absb(x)
 
