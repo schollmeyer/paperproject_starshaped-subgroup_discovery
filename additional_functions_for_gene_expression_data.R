f885810b91037db01bb3abdd0054289c9dd2e7a8
Georg Schollmeyer <69206011+schollmeyer@users.noreply.github.com>
2023-12-14 11:51:46 +0100

    Update additional_functions_for_gene_expression_data.R

--- additional_functions_for_gene_expression_data.R
+++ additional_functions_for_gene_expression_data.R
@@ -4,7 +4,7 @@ gene_filter <- function(data, x_percent=6 ){
   i <- NULL
   for(k in seq_len(ncol(data))){
     if( mean(data[,k]>2) < x_percent | mean(x[,k]>0)> 1-x_percent |  sd(x[,k])==0){i <- c(i,k)}
- }
+  }
 return(data[,-i])}
 
 scaling <- function(data){
@@ -12,5 +12,5 @@ scaling <- function(data){
   for(k in seq_len(ncol(data))){
     if(sd(data[,k])==0){print("Warning: standard deviation of 0 observed")}
     data[,k] <- (data[,k]-mean(data[,k]))/sd(data[,k])
-  }
+   }
 return(data)}
