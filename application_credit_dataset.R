6f31cb087a6c9f4e2270f3709f296a18ee338879
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2024-03-19 09:38:02 +0100

    GS: R script for credit data curated

--- application_credit_dataset.R
+++ application_credit_dataset.R
@@ -1,64 +1,73 @@
 # set working directory
 setwd("C:/Git/paperproject_starshaped-subgroup_discovery")
+# needed libraries
 library(foreign)
-library(rsubgroup)#
-dat_old <- read.arff("datasets/dataset_31_credit-g.arff")
+library(rsubgroup)
+# load the data set credit ( https://archive.ics.uci.edu/dataset/144/statlog+german+credit+data)
+# Thisis already the curated dataset, compare TODO
+# The variable sex (column 9) will be later deleted because it is actually not 
+# the variable sex but instead a combination of sex and marital status
 data(credit.data)
 dat <- credit.data
 dim(dat)
-# 
+# [1] 1000   21
 
-#generate formal context
+#generate formal context of covariates (sex (column 9) and credit status (column 21) are excluded)
 
 
 whole_context <- oofos:::get_auto_conceptual_scaling(dat[,-c(9,21)])
-#
-
+# For the classical subgroup discoery we make a sample split approach because 
+# optimizing under H0 is computationally very expensive
+set.seed(1234567)
 indexs <- sample((1:1000),size =500)
-training_context <- oofos:::get_auto_conceptual_scaling(dat[indexs,-c(9,21)])#whole_context[indexs,]
+training_context <- whole_context[indexs,]
 test_context <- whole_context[-indexs,]
-# remove duplicated columns
-#context <- t(unique(t(context)))
-dim(whole_context)
 
-# generate objective vector for optimization. This corresponds to the
+# generate objective vector for optimization. This corresponds 
+# (up to a multiplicative constant) to the
 # Piatetsky-Shapiro quality function for the target value credit-state==good
-objective <- oofos:::compute_objective(dat[indexs,],"class","good")#class","good")
+objective <- oofos:::compute_objective(dat[indexs,],"class","good")
 
 table(objective)
+
+# objective
+# -0.00628930817610063  0.00293255131964809 
+# 159                  341 
 #
-# 700 good states and 300 bad states
+# 341 good states and 159 bad states
 
 
 ############################################################################
-# Classical Subgroup Discovery
+# Classical Subgroup Discovery on a subset of size n=500
 ############################################################################
 
 
 # generate gurobi model for classical subgroup discovery
 model <- oofos:::optimize_on_context_extents(context=training_context,objective=objective,binary_variables="all" )
-
-model$A <- as.matrix(model$A)# optimize model under time constraint of 20 minutes for a first semioptimal solution
+# For later computations it is more effective to treat the constraint matrix 
+# not as a simple triplet matrix 
+model$A <- as.matrix(model$A)
+# add additional constraints that may help to speed up the optimization
+model <- oofos:::add_attr_antiimplications(model)
+#compute a preliminary result with timelimit of 10 minits
 result_1 <- gurobi::gurobi(model,list(timelimit=60*10))
 
 result_1$objval
-# [1] 0.257619
+# [1] 0.3223593
 
 
 result_1$runtime
-# [1] 2400.381
+# [1] 601.01
 
 # save result
 saveRDS(result_1,"results_credit_data/result_1.RDS")
 
 
 
-
-# add additional contsraints that are always valid for inetrordinally scaled variables x (implications of the form <= x \rightarrow not >= x+1 etc. 
-model_2 <- oofos:::add_attr_antiimplications(model)
-
-# use semioptimal solution to tighten the search space via optimistic estimates (in the style of ***)
-model_2 <- oofos:::add_sos_constraints(model_2,result_1$objval)
+# use semioptimal solution to tighten the search space via optimistic estimates
+# (in the style of M. Boley and H. Grosskreutz. Non-redundant subgroup discovery us-
+# ing a closure system.)
+model_2 <- oofos:::add_sos_constraints(model,result_1$objval)
 # add semioptimal solution as a start vector
 model_2$start <- round(result_1$x,2)
 result_2 <- gurobi::gurobi(model_2)
@@ -66,20 +75,60 @@ result_2 <- gurobi::gurobi(model_2)
 
 result_2$objval
 
-# [1] 0.3519048
-# no improvement in 20 minutes
+# [1] 0.3572364
 
-# Optimization without timelimit
-result_2 <- gurobi::gurobi(model_2)
+#overall runtie in hours 
+(result_1$runtime  + result_2$runtime)/3600
+# [1] 9.11968
 
-# save result
-# noch nicht geschen
-saveRDS(result_2,"results_credit_data/result_2.RDS")
+saveRDS(result_2,"result_2.RDS")
+
+############################################################################
+# Smple splitting test
+############################################################################
+
+extent <- round(result_2$x[(1:500)],2)
+intent <- round(result_2$x[-(1:500)],2)
+extent_test <- oofos:::compute_phi(intent,test_context)
+test_objective <- oofos:::compute_objective(dat[-indexs,],"class","good")
+observed_test_statistic <- sum(test_objective*extent_test)
+observed_test_statistic
+
+n_rep <-1000000
+h0 <- rep(0, n_rep)
+set.seed(1234567)
+for(k in (1:n_rep)){
+  h0[k] <- sum(sample(test_objective)*extent_test)
+}
+plot(ecdf(h0))
+mean(h0 >= observed_test_statistic)
+
+#0 
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+
+#  END OF CURATED PART
+
+############################################################################
+############################################################################
+############################################################################
+############################################################################
+############################################################################
 
 ############################################################################
 # Classical Subgroup Discovery on a subset of size n=500
 ############################################################################
+# Laufzeit ca 40 min
+#unter H0: Laufzeit ca. > 83864s
+# unter H0 mit gleichem Trick: 13040.84 s
+# mit bestem SOS constraint: 3274s
 
+result_2$objval
+#[1] 0.1326288
 set.seed(1234567)
 indexs <- sample((1:1000),size=500)
 sampled_context <- context[indexs,]
