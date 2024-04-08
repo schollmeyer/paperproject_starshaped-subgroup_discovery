f5f600455a948971904d5205da9213099717f159
schollmeyer <georg.schollmeyer@stat.uni-muenchen.de>
2024-04-08 17:45:22 +0200

    gs

---
+++ dataanalysis_treutlein_absb.R
@@ -0,0 +1,88 @@
+##########################################
+# Import data (code from hemberg-lab): https://github.com/hemberg-lab/scRNA.seq.datasets/blob/master/R/treutlein.R
+#########################################
+
+## set working directory accordingly !!!
+
+# load additional functions for computing stylized betweenness:
+source("used_stylized_betweenness_functions.R")
+# load additional functions needed for gene expression data (gene filter and scaling has to be applied):
+source("additional_functions_for_gene_expression_data.R")
+
+### DATA
+d <- read.table("datasets/nature13173-s4.txt")
+d <- t(d)
+genes <- d[,1][5:nrow(d)]
+# remove genes and bulk samples
+d <- d[,2:(ncol(d) - 2)]
+exprs_data <- as.data.frame(matrix(as.numeric(d[5:nrow(d),]), ncol = ncol(d)))
+rownames(exprs_data) <- genes
+colnames(exprs_data) <- d[1,]
+
+### ANNOTATIONS
+ann <- data.frame(cell_type1 = d[4,])
+rownames(ann) <- d[1,]
+
+
+
+############################################
+
+# manual construction of x and y:
+ x <- t(exprs_data)
+ x <- gene_filter(x)
+ x <- log2(1+x)
+ dim(x)
+ x <- scaling(x)
+
+ context <- oofos:::get_auto_conceptual_scaling(x)
+
+ # This context has VC dimension 80!!!
+
+ y <- ann$cell_type1
+ table(y)/length(y)
+
+ # y
+#  AT1      AT2       BP ciliated    Clara
+#  0.5125   0.1500   0.1625   0.0375   0.1375
+
+objective <- oofos:::compute_objective(data.frame(y=y),"y","AT1")
+
+absb <- get_absb(context)
+saveRDS(absb,"results_treutlein_absb/absb.RDS")
+
+gbsb <- readRDS("results_treutlein_gbsb/gbsb.RDS")
+
+plot(absb,gbsb)
+
+
+
+
+# absb:
+discovery <- oofos::discover_starshaped_subgroups(stylized_betweenness=absb,objective=objective,complexity_control=8)
+discovery$objval
+test_gbsb <- oofos:::compute_starshaped_distr_test(discovery, n_rep=20000)
+saveRDS(starshaped_discovery_gbsb,"results_treutlein_absb/starshaped_discovery.RDS")
+saveRDS(test_gbsb,"results_treutlein_absb/test.RDS")
+
+
+##### extensive analysis for gbsb
+set.seed(1234567)
+vc_dimensions <- seq(1,80,length.out=500)
+p_values <- rep(0,length(vc_dimensions))
+p_values_param <- rep(0,length(vc_dimensions))
+objvalues <- array(0,c(500,500))
+objval <- rep(0,500)
+for(k in (1:500)){
+  discovery <- oofos::discover_starshaped_subgroups(absb,objective=objective,complexity_control = vc_dimensions[k])
+  test <- oofos::compute_starshaped_distr_test(discovery,n_rep=500)
+  objvalues[k,] <- test$objvalues
+  objval[k] <- discovery$objval
+  p_values[k] <- test$p_value
+  p_values_param[k] <-  (discovery$objval-mean(test$objvalues))/sd(test$objvalues)
+  plot(vc_dimensions,(p_values_param))
+}
+
+saveRDS(p_values_param,"results_treutlein_absb/p_values_param.RDS")
+saveRDS(objvalues,"results_treutlein_absb/objvalues.RDS")
+saveRDS(objval,"results_treutlein_absb/objval.RDS")
+saveRDS(vc_dimensions,"results_treutlein_absb/vc_dimensions.RDS")
---
+++ results_treutlein_gbsb/relation_VC_dimension_angle_rademacher_complexity_gbsb.R
@@ -0,0 +1,134 @@
+compute_rademacher_complexity <-function (incidence,n_rep=10000) {
+  if (is.null(incidence)) {
+    return(list(complexity = 0))
+  }
+  n_rows <- nrow(incidence)
+  if (n_rows != ncol(incidence)) {
+    print("No square matrix")
+    return(NULL)
+  }
+  if (any(!incidence %in% c(0, 1))) {
+    print("Not all entries of the incidence matrix are in{0,1}")
+    return(NULL)
+  }
+  incidence <- oofos:::compute_transitive_hull(incidence)
+  diag(incidence) <- 1
+  incidence <- oofos:::compute_pseudoreduction(incidence)
+
+  if (n_rows == 0) {
+    return(list(complexity = 0))
+  }
+  if (n_rows == 1) {
+    return(list(complexity = 1))
+  }
+  result <- 0
+
+
+  model <- oofos:::get_model_from_quasiorder(t(incidence))
+  if (is.null(model)) {
+    model <- list(A = matrix(0, nrow = 1, ncol = n_rows),
+                  rhs = 1, sense = "<=")
+  }
+
+  model$lb <- rep(0, n_rows)
+  model$ub <- rep(1, n_rows)
+
+  model$modelsense <- "max"
+
+
+
+
+  for(k in seq_len(n_rep)){
+    rademacher_variables <- sample(c(-1,1),size=n_rows,replace=TRUE)
+    model$obj <- rademacher_variables#objective
+    b <- gurobi::gurobi(model, params = list(outputflag=0))
+    result <- result+b$objval
+
+  }
+
+  return(list(complexity=result/n_rep))
+}
+
+
+cut_incidence <- function(incidence, cut_value, complexity_measure=width,interval = stats::quantile(unique(as.vector(incidence)),
+                                                                                                    c(0, 1)))
+{
+  vc <- (complexity_measure(compute_quotient_order(compute_transitive_hull(incidence >=
+                                                                             interval[2]))))[[1]]
+  if (vc <= cut_value) {
+    return(incidence >= interval[2])
+  }
+  f <- function(C, incidence) {
+    complexity <- (complexity_measure(compute_quotient_order(compute_transitive_hull(incidence >=
+                                                                                       C))))[[1]]
+    return(complexity - cut_value)
+  }
+  ans <- stats::uniroot(f, interval = interval, incidence = incidence)
+  return(list(root=ans$root,incidence=compute_transitive_hull(incidence >= ans$root)))
+}
+
+
+n_rep <- 1000
+N <- 80
+rademacher_complexity <- rep(0,n_rep)
+vc_dimension <- rep(0,n_rep)
+cutting_value <- rep(0,n_rep)
+
+
+gbsb <- readRDS("gbsb.RDS")
+
+F_gbsb <- ecdf(gbsb)
+gbsb <- F_gbsb(gbsb)
+dim(gbsb) <- c(N,N,N)
+set.seed(1234567)
+for(k in (1:n_rep)){
+  l <- sample((1:N),size=1)
+
+
+  cut_value <- runif(1)
+
+  vc_dimension[k] <- oofos::compute_width(gbsb[l,,]>=cut_value)$width
+  #vc_dimension[k] <- sample((10:300),size=1)
+  #result <- cut_incidence(obsb[k,,],cut_value=vc_dimension[k], complexity_measure=width)
+  rademacher_complexity[k] <- compute_rademacher_complexity(gbsb[l,,]>=cut_value)$complexity#result$incidence)$complexity
+  cutting_value[k] <- cut_value
+  index <- which(vc_dimension>=2&vc_dimension<=78)
+  if(length(index)>1){plot(vc_dimension[index],rademacher_complexity[index])}
+
+}
+
+
+
+pplot <- ggplot(data=data.frame(vc_dimension=vc_dimension,cutting_value=cutting_value*180), aes(vc_dimension=vc_dimension, cutting_value=cutting_value))
+
+
+pplot + layer(
+  +     mapping = NULL,
+  +   position = "identity",
+  +   stat="identity",
+  +   geom = "point") ++labs(x = "VC dimension",y="cutting value in degrees")
+
+
+cor(rademacher_complexity[(1:k)],vc_dimension[(1:k)])
+
+# [1] 0.9993353
+
+summary(lm(rademacher_complexity[(1:k)]~vc_dimension[(1:k)]))
+
+# Call:
+#   lm(formula = rademacher_complexity[(1:k)] ~ vc_dimension[(1:k)])
+#
+# Residuals:
+#   Min      1Q  Median      3Q     Max
+# -6.7397 -0.3556  0.0203  0.2680  3.3644
+#
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)
+# (Intercept)         0.185801   0.049516   3.752 0.000207 ***
+#   vc_dimension[(1:k)] 0.546162   0.001093 499.493  < 2e-16 ***
+#   ---
+#   Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+#
+# Residual standard error: 0.8016 on 332 degrees of freedom
+# Multiple R-squared:  0.9987,	Adjusted R-squared:  0.9987
+# F-statistic: 2.495e+05 on 1 and 332 DF,  p-value: < 2.2e-16
