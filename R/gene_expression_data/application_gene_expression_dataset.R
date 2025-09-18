96d970ba68871d75cc6782cc414da912fae8df41
Schollmeyer <Georg.Schollmeyer@lmu.de>
2025-09-18 15:56:14 +0200

    GS

---
+++ R/gene_expression_data/application_gene_expression_dataset.R
@@ -0,0 +1,86 @@
+##########################################
+# Import data (code from hemberg-lab): https://github.com/hemberg-lab/scRNA.seq.datasets/blob/master/R/treutlein.R
+#########################################
+library(ggplot2)
+library(celltrackR)
+## set working directory accordingly !!!
+
+# load additional functions for computing stylized betweenness:
+source("../../used_stylized_betweenness_functions.R")
+# load additional functions needed for gene expression data (gene filter and scaling has to be applied):
+source("../../additional_functions_for_gene_expression_data.R")
+
+### DATA
+d <- read.table("../../datasets/nature13173-s4.txt")
+d <- t(d)
+genes <- d[,1][5:nrow(d)]
+# remove genes and bulk samples
+d <- d[,2:(ncol(d) - 2)]
+exprs_data <- as.data.frame(matrix(as.numeric(d[5:nrow(d),]), ncol = ncol(d)))
+rownames(exprs_data) <- genes
+colnames(exprs_data) <- d[1,]
+
+### ANNOTATIONS
+ann <- data.frame(cell_type1 = d[4,])
+rownames(ann) <- d[1,]
+
+
+
+############################################
+
+# manual construction of x and y:
+ x <- t(exprs_data)
+ x <- gene_filter(x)
+ x <- log2(1+x)
+ dim(x)
+ x <- scaling(x)
+
+ context <- oofos:::get_auto_conceptual_scaling(x)
+
+ dim(context)
+ #[1]     80 316836
+
+ # This context has VC dimension 80!!!
+
+ y <- ann$cell_type1
+ table(y)/length(y)
+
+ # y
+#  AT1      AT2       BP ciliated    Clara
+#  0.5125   0.1500   0.1625   0.0375   0.1375
+
+objective <- oofos:::compute_objective(data.frame(y=y),"y","AT1")
+
+#gbsb <- get_gbsb(x)
+#saveRDS(gbsb,"results_treutlein_gbsb/absb.RDS")
+gbsb <- readRDS("../../results_treutlein_gbsb/gbsb.RDS")
+dimnames(gbsb)[[1]] <- dimnames(gbsb)[[2]] <- dimnames(gbsb)[[3]] <- rownames(x)
+
+# gbsb:
+starshaped_discovery_gbsb <- oofos::discover_starshaped_subgroups(stylized_betweenness=gbsb,objective=objective, complexity_control = 7,complexity_measure=oofos::compute_width)
+starshaped_discovery_gbsb$objval
+set.seed(1234567)
+test_gbsb <- oofos:::compute_starshaped_distr_test(starshaped_discovery_gbsb, n_rep=100000)
+saveRDS(starshaped_discovery_gbsb,"results_treutlein/starshaped_discovery_gbsb")
+saveRDS(test_gbsb,"results_treutlein/test_gbsb")
+
+
+##### extensive analysis for gbsb
+n_rep <- 1000
+set.seed(1234567)
+vc_dimensions <- seq(1,80,length.out=80)
+p_values <- rep(0,length(vc_dimensions))
+p_values_param <- rep(0,length(vc_dimensions))
+objvalues <- array(0,c(80,n_rep))
+objval <- rep(0,80)
+for(k in (1:80)){
+  discovery <- oofos::discover_starshaped_subgroups(gbsb,objective=objective,complexity_control = vc_dimensions[k])
+  test <- oofos::compute_starshaped_distr_test(discovery,n_rep=n_rep)
+  objvalues[k,] <- test$objvalues
+  objval[k] <- discovery$objval
+  p_values[k] <- test$p_value
+  p_values_param[k] <-  (discovery$objval-mean(test$objvalues))/sd(test$objvalues)
+  plot(vc_dimensions,(p_values_param))
+}
+
+
